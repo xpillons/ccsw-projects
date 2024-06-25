716319ebc83cfb6f2bf3a28dfa4c68711f10f3e0
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-06-25 15:18:46 +0200

    slurm exporter

---
+++ projects/prometheus-exporters/specs/default/cluster-init/scripts/10_slurm_exporter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SPEC_FILE_ROOT="$script_dir/../files"
+
+source "$SPEC_FILE_ROOT/common.sh" 
+GO_VERSION=1.22.4
+SLURM_EXPORTER_VERSION=v1.5.1
+
+function install_slurm_exporter()
+{
+    wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+    rm go$GO_VERSION.linux-amd64.tar.gz
+
+    echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/bash.bashrc
+    source .bashrc
+
+    go install github.com/rivosinc/prometheus-slurm-exporter@$SLURM_EXPORTER_VERSION
+
+    cp $SPEC_FILE_ROOT/prometheus-slurm-exporter.service /etc/systemd/system
+    systemctl daemon-reload
+    systemctl enable prometheus-slurm-exporter
+    systemctl start prometheus-slurm-exporter
+
+}
+
+if is_scheduler ; then
+    install_slurm_exporter
+fi
