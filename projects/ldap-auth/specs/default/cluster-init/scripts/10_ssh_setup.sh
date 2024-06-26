a794d5445a1ccd3294b4cfe6b1718d8980f28343
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2024-06-26 14:49:16 +0200

    get SSH key from AD

--- projects/ldap-auth/specs/default/cluster-init/scripts/10_ssh_setup.sh
+++ projects/ldap-auth/specs/default/cluster-init/scripts/10_ssh_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-cp -f "$CYCLECLOUD_SPEC_PATH"/files/gen_sshkeys.sh /etc/profile.d # Copy setup script file
+#cp -f "$CYCLECLOUD_SPEC_PATH"/files/gen_sshkeys.sh /etc/profile.d # Copy setup script file
