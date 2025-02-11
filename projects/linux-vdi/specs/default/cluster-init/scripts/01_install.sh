46d5af2d7ae0f81d100f546a1158a6b3c3e09fd1
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-02-11 12:52:43 +0100

    add almalinux

--- projects/linux-vdi/specs/default/cluster-init/scripts/01_install.sh
+++ projects/linux-vdi/specs/default/cluster-init/scripts/01_install.sh
@@ -12,6 +12,10 @@ case $platform in
         DEBIAN_FRONTEND=noninteractive apt install -y ./turbovnc_${VERSION}_amd64.deb
         DEBIAN_FRONTEND=noninteractive apt-get install -y websockify
         ;;
+    almalinux)
+        dnf install -y https://github.com/TurboVNC/turbovnc/releases/download/${VERSION}/turbovnc_${VERSION}.x86_64.rpm
+        dnf install -y python3-websockify
+        ;;
     *)
         echo "Untested OS $platform $platform_family $platform_version"
         exit 0
