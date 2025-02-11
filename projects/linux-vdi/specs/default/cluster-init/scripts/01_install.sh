a656f378119cba6c9be1b0e95c6ec79471f173d4
Xavier Pillons (MSFT) <xpillons@microsoft.com>
2025-02-11 12:17:58 +0100

    add linux-vdi project

---
+++ projects/linux-vdi/specs/default/cluster-init/scripts/01_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+platform_family=$(jetpack config platform_family)
+platform=$(jetpack config platform)
+platform_version=$(jetpack config platform_version)
+
+VERSION=3.1.4
+
+case $platform in
+    ubuntu)
+        wget https://github.com/TurboVNC/turbovnc/releases/download/${VERSION}/turbovnc_${VERSION}_amd64.deb
+        DEBIAN_FRONTEND=noninteractive apt install -y ./turbovnc_${VERSION}_amd64.deb
+        DEBIAN_FRONTEND=noninteractive apt-get install -y websockify
+        ;;
+    *)
+        echo "Untested OS $platform $platform_family $platform_version"
+        exit 0
+        ;;
+esac
